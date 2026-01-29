18358854f7103984b8f39040cb86cebcbfd61e9f
baryandriamparany <baryandriamparany@gmail.com>
2026-01-29 09:05:23 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function scrollToMissions() {
+    document.getElementById("missions").scrollIntoView({
+        behavior: "smooth"
+    });
+}
+console.log("Page À propos chargée avec succès");
+
+console.log("Page Conseil & Assistance Technique chargée");
